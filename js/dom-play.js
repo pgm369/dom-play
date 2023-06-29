165ce069c55383725a14f7f54a183148621513d4
pgm369 <priissgm@gmail.com>
2023-06-29 10:16:25 -0700

    updates index

---
+++ js/dom-play.js
@@ -0,0 +1,17 @@
+//alert("Hi from DOM Play!");
+
+
+/*
+
+    Create an app that on click actors lines are highlighted
+
+    With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+    Un-highlight (normal or otherwise) all lines from all other actors in the play
+   
+    The function(s) created to do the work must accommodate any number of actors, and any number of additional lines added to the page
+   
+    HTML "hooks" may be added to the page for proper operation
+
+*/
+
+document.querySelector(".hamlet").style.backgroundColor = "pink"
